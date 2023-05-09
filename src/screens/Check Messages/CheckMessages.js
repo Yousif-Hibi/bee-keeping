e0e214a9b6259244e7cd3f0ce5a9e6d34f63a90c
Yousif-Hibi <yousif.hibi@gmail.com>
2023-05-09 17:42:39 +0300

    masseges

--- src/screens/Check Messages/CheckMessages.js
+++ src/screens/Check Messages/CheckMessages.js
@@ -1,21 +1,88 @@
 import React, { useState } from 'react';
-// import React, { useLayoutEffect } from "react";
-import { FlatList, Text, View, TouchableHighlight, Image } from "react-native";
+import { Text, View, TouchableOpacity, Image, Modal, ImageBackground ,TextInput } from "react-native";
 import styles from "./styles";
-import { ImageBackground } from 'react-native';
 import { StatusBar } from 'react-native';
 
-
 export default function CheckMessages() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isModal2Visible, setIsModal2Visible] = useState(false);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  function handleSend() {
+    // Send the message to the backend server or API
+    // Then add the message to the list of messages
+    setMessages([...messages, message]);
+    setMessage('');
+  }
+  function textPress() {
+    setIsModal2Visible(true);
+
+
+  }
+  function imgPress() {
+    setIsModalVisible(true);
+  }
+
+  function closeModal() {
+    setIsModalVisible(false);
+    setIsModal2Visible(false);
+  }
+
   return (
     <ImageBackground
       source={require('../../../assets/beesbackground.jpg')}
       style={styles.background}
     >
-      <View style={styles.container}>
-        <Text style={styles.title}>Messages Page</Text>
-        <StatusBar style="auto" />
-      </View>
+      <TouchableOpacity style={styles.square}>
+        <TouchableOpacity onPress={imgPress}>
+          <Image source={require('../../../assets/image.png')} style={styles.image} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={textPress}>
+          <Text style={styles.text}>Hello World</Text>
+        </TouchableOpacity>
+        <Modal animationType='fade' transparent={true} visible={isModalVisible}>
+          <TouchableOpacity style={styles.modalBackground} onPress={closeModal}>
+            <View style={styles.modalContent}>
+              <Image source={require('../../../assets/beeColony.jpg')} style={styles.imagePop} />
+              <TouchableOpacity onPress={closeModal}>
+                <Text style={styles.closeButton}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+        <Modal animationType='fade' transparent={true} visible={isModal2Visible}>
+          <TouchableOpacity style={styles.modalBackground} onPress={closeModal}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalText}>This is a popup message!</Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Type your message here"
+                  value={message}
+                  onChangeText={setMessage}
+                />
+                
+
+               
+                  
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
+                      <Text style={styles.sendButtonText}>Send</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+                      <Text style={styles.closeButtonText}>Close</Text>
+                    </TouchableOpacity>
+                  </View>
+              
+
+
+
+              </View>
+            </View>
+          </TouchableOpacity>
+        </Modal>
+      </TouchableOpacity>
     </ImageBackground>
   );
 }
