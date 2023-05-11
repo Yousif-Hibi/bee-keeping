6cc37df61b4c05afd3f4cd6be787c6446fdf3296
samerrkh <s.khair.2000@gmail.com>
2023-05-11 17:03:01 +0300

    colAccInfo done

--- src/screens/ColonyAccountInfo/ColAccinfo.js
+++ src/screens/ColonyAccountInfo/ColAccinfo.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 import styles from "./styles";
-import { StatusBar } from 'react-native';
+import { StatusBar } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 export default function NumberChanger() {
   const [count, setCount] = useState(0);
 
@@ -10,18 +18,39 @@ export default function NumberChanger() {
   };
 
   const handleDecrement = () => {
-    setCount((prevCount) => prevCount - 1);
+    if (count > 0) {
+      setCount((prevCount) => prevCount - 1);
+    }
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handleIncrement}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
-      <Text style={styles.count}>{count}</Text>
-      <TouchableOpacity style={styles.button} onPress={handleDecrement}>
-        <Text style={styles.buttonText}>-</Text>
-      </TouchableOpacity>
-    </View>
+    <ImageBackground
+      source={require("../../../assets/beesbackground.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <MaterialCommunityIcons
+            name="home"
+            size={30}
+            onPress={() => navigation.navigate("AdminInfo")}
+            style={styles.homeIcon}
+          />
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <Text style={styles.title}>Choose how many colonies</Text>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleIncrement}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <Text style={styles.count}>{count}</Text>
+        <TouchableOpacity style={styles.button} onPress={handleDecrement}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.submitButton}>
+          <Text style={styles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 }
