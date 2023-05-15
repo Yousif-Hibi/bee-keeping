7a5b613e2919e8297b321fb78268cc9c612c6d63
amirfagh <127864217+amirfagh@users.noreply.github.com>
2023-05-15 12:09:14 +0300

    presenntday

--- src/screens/ColAccInfoScreen/ColAccIInfo.js
+++ src/screens/ColAccInfoScreen/ColAccIInfo.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet,ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 import styles from "./styles";
-import { StatusBar } from 'react-native';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-export default function NumberChanger() {
+import { StatusBar } from "react-native";
+export default function ColAccInfo({ navigation }) {
   const [count, setCount] = useState(0);
 
   const handleIncrement = () => {
@@ -11,37 +17,39 @@ export default function NumberChanger() {
   };
 
   const handleDecrement = () => {
-    setCount((prevCount) => prevCount - 1);
+    if (count > 0) {
+      setCount((prevCount) => prevCount - 1);
+    }
   };
 
   return (
     <ImageBackground
-    source={require("../../../assets/beesbackground.jpg")}
-    style={styles.background}
-  >
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <MaterialCommunityIcons
-          name="home"
-          size={30}
-          onPress={() => navigation.navigate("AdminInfo")}
-          style={styles.homeIcon}
-        />
-        <View style={{ justifyContent: "center", alignItems: "center" }}>
-          <Text style={styles.title}>Choose how many colonies</Text>
+      source={require("../../../assets/beesbackground.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <MaterialCommunityIcons
+            name="home"
+            size={30}
+            onPress={() => navigation.navigate("AdminInfoScreen")}
+            style={styles.homeIcon}
+          />
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <Text style={styles.title}>Choose how many colonies</Text>
+          </View>
         </View>
+        <TouchableOpacity style={styles.button} onPress={handleIncrement}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <Text style={styles.count}>{count}</Text>
+        <TouchableOpacity style={styles.button} onPress={handleDecrement}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.submitButton}>
+          <Text style={styles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.button} onPress={handleIncrement}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
-      <Text style={styles.count}>{count}</Text>
-      <TouchableOpacity style={styles.button} onPress={handleDecrement}>
-        <Text style={styles.buttonText}>-</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.submitButton}>
-        <Text style={styles.submitButtonText}>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  </ImageBackground>
+    </ImageBackground>
   );
 }
